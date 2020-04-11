814877443adc20aa17beadd1bc906db54812c71e
Cody Chinn <cdchinn14@gmail.com>
2020-04-11 16:56:38 -0400

    Added hyperlinks to nav and a non funtioning tag button

--- beam/src/Components/NavMenu.jsx
+++ beam/src/Components/NavMenu.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import { ReactComponent as Logo } from "../logo.svg";
 
 export default function NavMenu () {
     
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-            <img
-                alt=""
-                src="/logo192.png"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-            />{' '}
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+            <Logo
+            alt=""
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            />
             Beam
-            </Navbar.Brand>
-        </Navbar>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="#profile">Profile</Nav.Link>
+            <Nav.Link href="#add-post">Add Post</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+
     )
 
 }
--- beam/src/Components/Post.jsx
+++ beam/src/Components/Post.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import "../Post.css";
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 export default function Post () {
 
     return (
-        <Card className="post-container col-md-6">
-            <Card.Body>
-                <Card.Title>
-                <img 
-                    src="../default_profile.png" 
-                    className="post-pic" 
-                    alt="profile"  />
-                    Cory Chinn
-                </Card.Title>
-                <Card.Text>
-                    Check out some of my new music!
-                </Card.Text>  
-                <div> 
+        <div className="post-container col-md-6">
+            <Card>
+                <Card.Body>
+                    <Card.Title>
                     <img 
-                        src="../note.jpg"
-                        className="post-content-img" 
-                        alt="album cover"/>       
-                    <div>
-                        Album: mixtape <br />
-                        Track: UFO <br />
-                        Artist: Cory Chinn <br />
+                        src="../default_profile.png" 
+                        className="post-pic" 
+                        alt="profile"  />
+                        Cory Chinn
+                    <div className="post-tag">
+                        <Button variant="secondary">Engineer</Button>{' '}
                     </div>
-                </div>   
-            </Card.Body>
-        </Card>
+                    </Card.Title>
+                    <Card.Text>
+                        Check out some of my new music!
+                    </Card.Text>  
+                    <div> 
+                        <img 
+                            src="../note.jpg"
+                            className="post-content-img" 
+                            alt="album cover"/>       
+                        <div>
+                            Album: mixtape <br />
+                            Track: UFO <br />
+                            Artist: Cory Chinn <br />
+                        </div>
+                    </div>   
+                </Card.Body>
+            </Card>
+        </div>
+
     )
 }

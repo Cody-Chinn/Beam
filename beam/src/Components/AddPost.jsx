db497f8db5a3abf92754abf00b57512e8a5f3b2a
Cody Chinn <cdchinn14@gmail.com>
2020-04-19 16:59:48 -0400

    Can now add to database

--- beam/src/Components/AddPost.jsx
+++ beam/src/Components/AddPost.jsx
@@ -1,54 +1,177 @@
-import React from "react";
-import Form from 'react-bootstrap/Form';
+import React, {useState} from "react";
 import Button from 'react-bootstrap/Button'
 import '../Styling Sheets/AddPost.css'
+import { db } from '../firebase.js'
  
 const AddPost = () => {
+
+  const [post, setPost] = useState({
+    name: '',
+    track: '',
+    artist: '',
+    album: '',
+    description: '',
+    link: '',
+    tags: []
+  });
+
+  const handleSubmit = e => {
+
+    // Need to convert the string into individual tags
+    post.tags = document.getElementById('tags').value.split(" ");
+
+    e.preventDefault();
+    db.collection('Posts').add(post); // update
+    setPost({
+      name: '',
+      track: '',
+      artist: '',
+      album: '',
+      description: '',
+      link: '',
+      tags: []
+    });
+  };
+
+  const handleChange = e => {
+    setPost({ ...post, [e.target.name]: e.target.value });
+  };
   
     return (
       <div>
         <div className="add-post-container col-md-6">
-          <Form>
+          <form onSubmit={handleSubmit}>
+          <label className='active' htmlFor='name'>
+            Name
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={post.name}
+              onChange={handleChange}
+              placeholder='first name last name'
+              className='validate'
+              required
+            />
+          </div>
 
-          <Form.Group controlId="formName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="textarea" rows="1" />
-            </Form.Group>
+          <br />
 
-            <Form.Group controlId="formArtist">
-              <Form.Label>Artist</Form.Label>
-              <Form.Control type="textarea" rows="1" />
-            </Form.Group>
+          <label className='active' htmlFor='artist'>
+            Artist
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='artist'
+              name='artist'
+              value={post.artist}
+              onChange={handleChange}
+              placeholder='stage name'
+              className='validate'
+              required
+            />
+          </div>
 
-            <Form.Group controlId="formTrack">
-              <Form.Label>Track</Form.Label>
-              <Form.Control type="textarea" rows="1" />
-            </Form.Group>
+          <br />
 
-            <Form.Group controlId="formAlbum">
-              <Form.Label>Album</Form.Label>
-              <Form.Control type="textarea" rows="1" />
-            </Form.Group>
+          <label className='active' htmlFor='track'>
+            Track
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='track'
+              name='track'
+              value={post.track}
+              onChange={handleChange}
+              placeholder='Bohemian Rhapsody'
+              className='validate'
+              required
+            />
+          </div>
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Post description</Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
+            <br />
+          
+          <label className='active' htmlFor='album'>
+            Album
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='album'
+              name='album'
+              value={post.album}
+              onChange={handleChange}
+              placeholder='mixtape'
+              className='validate'
+              required
+            />
+          </div>
 
-            <Form.Group controlId="formLink">
-              <Form.Label>Link</Form.Label>
-              <Form.Control type="textarea" rows="1" />
-            </Form.Group>
+          <br />
+          
+          <label className='active' htmlFor='description'>
+            Track Description
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='description'
+              name='description'
+              value={post.description}
+              onChange={handleChange}
+              placeholder='A cool tune to listen to in the car'
+              className='validate'
+              required
+            />
+          </div>
 
-            <Form.Group controlId="formTags">
-              <Form.Label>Tags</Form.Label>
-              <Form.Control type="textarea" rows="1" />
-            </Form.Group>
+          <br />
+          
+          <label className='active' htmlFor='link'>
+            link
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='link'
+              name='link'
+              value={post.link}
+              onChange={handleChange}
+              placeholder='youtube/spotify links'
+              className='validate'
+              required
+            />
+          </div>
+
+
+          <br />
+          
+          <label className='active' htmlFor='tags'>
+            tags
+          </label>
+          <div className='input-field'>     
+            <input
+              type='text'
+              id='tags'
+              name='tags'
+              value={post.tags}
+              onChange={handleChange}
+              placeholder='beats, country, etc.'
+              className='validate'
+              required
+            />
+          </div>
+
+          <br />
 
             <Button variant="primary" type="submit">
               Submit
             </Button>
-          </Form>
+          </form>
         </div>
       </div>
     );
